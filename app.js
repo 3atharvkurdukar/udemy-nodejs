4887df4b331c42fd3d08f1f4443659a9a5eb494c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-07 20:36:05 +0530

    Use of custom modules in code

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 console.log('App started!');
 
-const fs = require('fs');
-const os = require('os');
+// const fs = require('fs');
+// const os = require('os');
 
-let user = os.userInfo();
+// let user = os.userInfo();
 
 // Async Method
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
-    if (err) {
-        console.log(err);
-    }
-});
+// fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
 
 // Synchronous Method
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+
+const notes = require('./notes.js');
+
+let res = notes.addNote();
+
+let sum = notes.add(15, 80);
+console.log('Sum = ', notes.add(15, 80));
 
---
+++ notes.js
@@ -0,0 +1,8 @@
+module.exports.addNote = () => {
+    console.log('New Note');
+    return 'New Note';
+};
+
+module.exports.add  = (a, b) => a + b;
+
+
