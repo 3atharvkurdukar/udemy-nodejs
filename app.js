0873a0b574c518e727905efd4b87feda2e11bc5f
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-10 19:38:30 +0530

    Added weather callback

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
     .options({
@@ -13,11 +14,21 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
+
 geocode.geocodeAddress(argv.address, (errorMsg, results) => {
     if (errorMsg) {
         console.log(errorMsg);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log(`Address:     ${results.address}`);
+        weather.getWeather(results.latitude, results.longitude, (errorMsg, results) => {
+            if (errorMsg) {
+                console.log(errorMsg);
+            } else {
+                console.log(`Summary:     ${results.summary}`);
+                console.log(`Temperature: ${results.temperature}°C`);
+                console.log(`Wind Speed:  ${results.windSpeed} km/h`);
+            }
+        });
     }
 });
 
