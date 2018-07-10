71b8776fd55b892f57420eb3a8217d4d6b927e92
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-10 15:54:31 +0530

    Data Abstraction

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -13,25 +13,11 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
-
-const encodedAddress = encodeURIComponent(argv.address);
-
-request(
-    `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    {
-        json: true
-    },
-    (error, response, body) => {
-        if (error) {
-            console.log('Unable to connect to Google servers');
-        } else if (body.status === 'ZERO_RESULTS') {
-            console.log('Unable to find address');
-        } else if (body.status === 'OK') {
-            console.log(`Address:   ${body.results[0].formatted_address}`);
-            console.log(`Latitude:  ${body.results[0].geometry.location.lat}`);
-            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-        } else if (body.status === 'OVER_QUERY_LIMIT') {
-            console.log('The server is flooding! Please try after some time...');
-        }
+geocode.geocodeAddress(argv.address, (errorMsg, results) => {
+    if (errorMsg) {
+        console.log(errorMsg);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
-)
+});
+
