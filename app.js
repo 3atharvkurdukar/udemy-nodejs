e15f1aec4bddecd6782135691b01d4e48d7de2a5
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-10 14:46:21 +0530

    Getting lcoation info from Google API

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
 
 request(
-    'http://maps.googleapis.com/maps/api/geocode/json?address=shreeram%20residency%20kothrud%20pune',
+    `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     {
         json: true
     },
     (error, response, body) => {
-        console.log(body);
+        if (error) {
+            console.log('Unable to connect to Google servers');
+        } else if (body.status === 'ZERO_RESULTS') {
+            console.log('Unable to find address');
+        } else if (body.status === 'OK') {
+            console.log(`Address:   ${body.results[0].formatted_address}`);
+            console.log(`Latitude:  ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        } else if (body.status === 'OVER_QUERY_LIMIT') {
+            console.log('The server is flooding! Please try after some time...');
+        }
     }
 )
