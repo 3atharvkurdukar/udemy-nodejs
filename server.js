e23099e6fea90eb35ff6808787607197ddd48327
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-12 16:49:16 +0530

    Using Template Engine

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello express</h1>');
-    res.send('<h1>Hello express!</h1>');
+    res.render('home.hbs', {
+        welcomeMessage: 'Welcome to my website!',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send({
-        name: 'John',
-        age: 23
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
     });
 });
 
