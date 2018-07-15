1d2d1b88dfbd893f1e556416c0f4ea1007f349fd
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-15 17:46:25 +0530

    Understanding _id field

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost', (err, client) => {
     if (err) {
@@ -8,14 +9,21 @@ MongoClient.connect('mongodb://localhost', (err, client) => {
 
     const db = client.db('TodoApp');
     db.collection('Todos').insertOne({
-        text: 'Something to do',
-        completed: false
+        text: 'Something else',
+        completed: true
     }, (err, result) => {
         if (err) {
             return console.log('Unable to insert todo', err);
         }
         console.log(JSON.stringify(result.ops, undefined, 2));
+        //  _id is an automatically generated field with built-in timestamp
+        // getTimestamp() can be used to return Date object of _id
+        console.log(result.ops[0]._id.getTimestamp());
     });
+
+    // We can generate our own IDs using ObjectID class
+    const obj = new ObjectID();
+    console.log(obj);
 
     client.close();
 });
