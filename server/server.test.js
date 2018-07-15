bf069c27783d33f447e1043212d44a2415fe11ae
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-15 12:13:03 +0530

    Grouping tests using describe

--- server/server.test.js
+++ server/server.test.js
@@ -2,22 +2,27 @@ const request = require('supertest');
 const expect = require('expect');
 const app = require('./server').app;
 
-it('should return hello world response', (done) => {
-        request(app)
-        .get('/')
-        .expect(200)
-        .expect('Hello World')
-        .end(done);
-});
-
-it('should have user info', (done) => {
-    request(app)
-        .get('/users')
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Atharv',
-                age: 19
-            });
-        })
-        .end(done);
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(200)
+                .expect('Hello World')
+                .end(done);
+        });
+    });
+    describe('GET  /users', () => {
+        it('should have user info', (done) => {
+            request(app)
+                .get('/users')
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Atharv',
+                        age: 19
+                    });
+                })
+                .end(done);
+        });
+    });
 });
