b5f7da111cf630dc3eaea44288b6f6f5cf7124f6
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-17 12:10:54 +0530

    Bug fixes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 // const {User} = require('./models/user');
 
