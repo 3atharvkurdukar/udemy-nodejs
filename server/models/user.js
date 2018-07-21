254f6bae935727ce6739133f70478b9639a924d3
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-07-21 14:29:08 +0530

    Adding POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -56,6 +56,23 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    const User = this;
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (!res) {
+                    reject();
+                }
+                resolve(user);
+            });
+        });
+    });    
+};
+
 UserSchema.methods.generateAuthToken = function () {
     const user = this;
     const access = 'auth';
